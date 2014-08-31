d84976925690a379d5fc833a91ece986e66a8b0c
Petr Faitl <themadczech@gmail.com>
2014-08-31 20:21:47 +1200

    detail fragment
    separated detail fragemnt

--- app/src/main/java/app/sunshine/android/lonerunner/info/sunshine/DetailActivity.java
+++ app/src/main/java/app/sunshine/android/lonerunner/info/sunshine/DetailActivity.java
@@ -1,17 +1,10 @@
 package app.sunshine.android.lonerunner.info.sunshine;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ShareActionProvider;
-import android.widget.TextView;
 
 
 public class DetailActivity extends Activity
@@ -55,79 +48,6 @@ public class DetailActivity extends Activity
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public class DetailFragment extends Fragment
-    {
-        private static final String SHARED_HASHTAG = "#SunshineApp";
-        private String mReceivedForecast;
-        private ShareActionProvider mShareActionProvider;
-
-        public DetailFragment()
-        {
-
-            setHasOptionsMenu(true);
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState)
-        {
-
-            Intent detailIntent = getActivity().getIntent();
-            Bundle extras = detailIntent.getExtras();
-            mReceivedForecast = extras.getString(Intent.EXTRA_TEXT);
-            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            if (detailIntent != null && extras != null)
-            {
-                TextView displayedText = (TextView) rootView.findViewById(R.id.detail_forecast_text);
-                displayedText.setText(mReceivedForecast);
-            }
-
-
-            return rootView;
-        }
-
-        @Override
-        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
-        {
-            // Inflate menu resource file.
-            getMenuInflater().inflate(R.menu.detailsfragment, menu);
-
-            // Locate MenuItem with ShareActionProvider
-            MenuItem item = menu.findItem(R.id.action_share);
-
-            // Fetch and store ShareActionProvider
-            mShareActionProvider = (ShareActionProvider) item.getActionProvider();
-
-            // Return true to display menu
-            setShareIntent();
-            super.onCreateOptionsMenu(menu,getMenuInflater());
-        }
-
-        private void setShareIntent()
-        {
-            Intent shareIntent = createShareIntent();
-            if (mShareActionProvider != null)
-            {
-                mShareActionProvider.setShareIntent(shareIntent);
-            }
-        }
-
-        private Intent createShareIntent()
-        {
-            Intent shareIntent = new Intent(Intent.ACTION_SEND);
-            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-            shareIntent.setType("text/plain");
-            shareIntent.putExtra(Intent.EXTRA_TEXT, mReceivedForecast + " " + SHARED_HASHTAG);
-            return shareIntent;
-        }
-
-
     }
 
 
